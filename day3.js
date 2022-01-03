b89bf404e6d1fcf9ed382881fe292c650ecd1031
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-01-03 14:07:10 -0800

    Add a new exercise

---
+++ day3.js
@@ -0,0 +1,5 @@
+// What will the following code  print to the console?
+
+console.log("hello".toUpperCase()); // HELLO
+console.log("Hey".startsWith("H")); // true
+console.log("goodbye".toLowerCase()); // goodbye
