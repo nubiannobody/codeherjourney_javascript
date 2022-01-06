0201654f06c68d52c4a96c71e4a8ef65c6017a5c
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-01-05 21:26:15 -0800

    Add a new exercise

---
+++ day5.js
@@ -0,0 +1,10 @@
+// What will the following code print to the console?
+
+let dayInJanuary = 4;
+let daysUntilFebruary = 28;
+
+dayInJanuary++; // 5
+daysUntilFebruary--; // 27
+
+console.log(dayInJanuary);
+console.log(daysUntilFebruary);
