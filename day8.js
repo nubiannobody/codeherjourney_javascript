86360242a1540648092ce4deb6a2105a00d68036
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-01-10 23:22:43 -0800

    Add a new file

---
+++ day8.js
@@ -0,0 +1,12 @@
+// Conditional statements in JavaScript
+// What will the following code print to the console?
+
+let gymWorkoutComplete = true;
+
+gymWorkoutComplete = false;
+
+if(gymWorkoutComplete) {
+    console.log('Yes! Totally smashed my workout today');
+} else {
+    console.log('Oop, going later on today...');
+}
