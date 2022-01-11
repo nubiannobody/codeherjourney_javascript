42f7b24178163195e0a5857878e9c03fe6962fbc
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-01-10 23:16:46 -0800

    Add new files

---
+++ day10.js
@@ -0,0 +1,7 @@
+/* Logical operators time!
+What will the following code print to the console? */
+
+console.log((1 + 1 === 2) && (2 + 2 === 4)); // true
+console.log((1 > 9) && (5 !== 6)); // false
+console.log((1 + 1 === 2) || (2 < 1)) // true
+console.log((0 === 10) || (1 + 1 === 1)) // false
---
+++ day9.js
@@ -0,0 +1,8 @@
+// What will the following code print to the console?
+
+let gamePoints = 15;
+if (gamePoints > 15) {
+    console.log('Well done on passing this round!!!');
+} else {
+    console.log('Keep playing!');
+}
