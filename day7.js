02822281d79e1e15467e06638769f56022e1bde6
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-01-10 23:29:46 -0800

    Add a new file

---
+++ day7.js
@@ -0,0 +1,11 @@
+// Have you heard of the typeof operator?
+// What will the following code print to the console?
+
+const programmingLanguage = 'JavaScript';
+console.log(typeof programmingLanguage);
+
+const JavaScriptJanuaryChallengeDay = 7;
+console.log(typeof JavaScriptJanuaryChallengeDay);
+
+const enjoyTheChallenge = true;
+console.log(typeof enjoyTheChallenge);
