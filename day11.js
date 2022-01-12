083ca90059e0951eebd06260dc216f4e6b007fb4
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-01-11 23:32:27 -0800

    Add a new exercise

---
+++ day11.js
@@ -0,0 +1,9 @@
+// What will the following code print to the console?
+
+let favouriteCat = '';
+
+if (favouriteCat) {
+    console.log(`Your favourite cat is ${favouriteCat}`);
+} else {
+    console.log('You MUST pick your favourite cat!!!');
+}
