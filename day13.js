afe4d3ce98b44698aab83037857e91797b539ce6
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-01-13 23:08:52 -0800

    Add a new exercise

---
+++ day13.js
@@ -0,0 +1,7 @@
+/* Have you seen ternary operators in JavaScript?
+What will the following code print to the console? */
+
+let januaryChallenge = 'JavaScript January';
+
+januaryChallenge ? console.log(`The challenge I'm doing is called ${januaryChallenge}`) 
+: console.log(`I am not doing a challenge at the moment!`);
