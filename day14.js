4625c1ee657b7d6c9e7d768b52405c416bcd6a53
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-01-14 20:48:45 -0800

    Add a new exercise

---
+++ day14.js
@@ -0,0 +1,15 @@
+/* Have you heard of switch statements in JavaScript?
+What will the following code print to the console? */
+
+let programmingLanguage = "PHP";
+
+switch (programmingLanguage) {
+    case "JavaScript":
+        console.log("Courses start from $4");
+        break;
+    case "Python":
+        console.log("Courses start from $5");
+        break;
+    default:
+        console.log("No courses currently available");
+}
