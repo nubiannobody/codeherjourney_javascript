68963a60ded927df71030a233de34f096846db31
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-01-15 10:29:11 -0800

    Add a new exercise

---
+++ day15.js
@@ -0,0 +1,6 @@
+/* When we run the following line of code, nothing is being displayed 
+on the console. How de we display the statement in the console.log?? */
+
+function sayThanks() {
+    console.log("Thank you for doing the JavaScript January challenge!");
+}
