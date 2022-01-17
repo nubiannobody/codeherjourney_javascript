dc28f53ea116da85bbf2ce828d2c33ffc5c64ece
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-01-16 19:45:36 -0800

    Add a new exercise

---
+++ day16.js
@@ -0,0 +1,9 @@
+/* Have you used default parameters in JavaScript?
+What does the following print to the console? */
+
+function greeting(name = 'stranger'){
+    console.log(`Hello, ${name}!`)
+}
+
+greeting('Amber');
+greeting();
