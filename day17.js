ac4573970b7dc4e87bb26e1ca85ca7903d607506
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-01-17 23:59:27 -0800

    Add new

---
+++ day17.js
@@ -0,0 +1,9 @@
+/* Have you used arrow functions in JavaScript?
+What does the following print to the console? */
+
+const rectangleArea = (width, height) => {
+    let area = width * height;
+    return area;
+};
+
+console.log(rectangleArea(5,3));
