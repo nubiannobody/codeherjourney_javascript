e4715117b19f567b74d64e95850b6524f3f66f8b
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-01-19 23:51:09 -0800

    Add new exercise

---
+++ day19.js
@@ -0,0 +1,10 @@
+// What will the following code print to the console?
+
+const capitalCity = 'Canberra';
+
+const logAustraliaCapitalCity = () => {
+    let country = 'Australia';
+    return `The capital of ${country} is ${capitalCity}`;
+}
+
+console.log(logAustraliaCapitalCity());
