72e6b36ac79b15bb9257f3008d5d3763a863e949
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-01-21 23:36:36 -0800

    Add new exercise

---
+++ day21.js
@@ -0,0 +1,18 @@
+// What will the following code print to the console?
+
+const holidayTrip = () => {
+    const destination = 'Paris, France';
+    const passportPacked = false;
+    const passengers = 2;
+
+    return destination;
+
+};
+
+//console.log(holidayTrip()); Paris, France
+console.log(`Next stop: ${destination}`); // referenceError
+
+
+
+
+
