3e271fed9a47eb474fe572781403f558a51d8730
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-01-26 22:21:51 -0800

    Add new exercise

---
+++ day26.js
@@ -0,0 +1,7 @@
+/* Have you come across nested arrays? 
+What will the following code print to the console? */
+
+const numberClusters = [[1,2], [3,4], [5,6]]
+
+const target = numberClusters[2][1] // 
+console.log(target); // 6
