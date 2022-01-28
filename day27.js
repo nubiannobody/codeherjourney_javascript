8313c67e5a2e7d54932ff121a85190564250a061
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2022-01-27 22:51:33 -0800

    Add new exercise

---
+++ day27.js
@@ -0,0 +1,8 @@
+/* Have you come across the .shift() method?
+What will the following code print to the console? */
+
+const techStack = ['JavaScript', 'React', 'Redux'];
+techStack.shift();
+console.log(techStack);
+techStack = ['Node', 'TypeScript', 'GraphQL'];
+console.log(techStack);
